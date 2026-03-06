effd2326312033de93c58b7e1242bf7dfd12a99e
Jedsada Sukluan <2210511101031@live4.utcc.ac.th>
2026-03-06 11:18:12 +0700

    Bug fix#3 – revise candidate no. validation

--- validate.js
+++ validate.js
@@ -9,7 +9,8 @@ function checkSID() {
 
 function checkCandiNo() {
   let candi = (document.getElementById("candi").value).trim();
-  if (isNaN(candi) || candi < 1) {
+
+  if (isNaN(candi) || candi < 1 || candi > 10) {
     return false;
   } else {
     return true;
@@ -17,18 +18,18 @@ function checkCandiNo() {
 }
 
 function validateForm(){
-	if(!checkSID()){
-	  alert("Invalid value for Student ID!");
-	  document.getElementById("sid").focus();
-	  return false;
-	}else{
-		if(!checkCandiNo()){
-		  alert("Invalid value for Candidate No!!");
-		  document.getElementById("candi").focus();
-		  return false;
-		}else{
-			alert("Your input data passes validation!!");
-			return true;
-		}
-	}
+  if(!checkSID()){
+    alert("Invalid value for Student ID!");
+    document.getElementById("sid").focus();
+    return false;
+  }else{
+    if(!checkCandiNo()){
+      alert("Invalid value for Candidate No!! (1-10 only)");
+      document.getElementById("candi").focus();
+      return false;
+    }else{
+      alert("Your input data passes validation!!");
+      return true;
+    }
+  }
 }
